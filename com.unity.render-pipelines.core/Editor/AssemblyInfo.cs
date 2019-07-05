c8ebd867ceb4eff63712cf18ced359c7bef319ae
Julien Ignace <julien@unity3d.com>
2019-07-05 17:34:25 +0200

    Removed commented code

--- com.unity.render-pipelines.core/Editor/AssemblyInfo.cs
+++ com.unity.render-pipelines.core/Editor/AssemblyInfo.cs
@@ -1,6 +1,2 @@
 using System.Runtime.CompilerServices;
 
-//[assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor")]
-//[assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor.Tests")]
-//[assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Runtime.Tests")]
-
--- com.unity.render-pipelines.core/Runtime/AssemblyInfo.cs
+++ com.unity.render-pipelines.core/Runtime/AssemblyInfo.cs
@@ -1,5 +1,1 @@
 using System.Runtime.CompilerServices;
-
-//[assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Runtime")]
-//[assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor")]
-//[assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor.Tests")]
