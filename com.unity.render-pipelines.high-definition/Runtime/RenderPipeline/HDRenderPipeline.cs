60f24fa12b973fa130f15ac9b446deafc6ed707c
Paul Demeulenaere <pauld@unity3d.com>
2019-07-11 15:11:02 +0200

    Remove reflection code, it isn't needed anymore

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.cs
@@ -987,37 +987,6 @@ namespace UnityEngine.Rendering.HighDefinition
             }
         }
 
-        private static readonly System.Reflection.MethodInfo k_VFXManagerPrepareCamera = typeof(UnityEngine.VFX.VFXManager).GetMethod("PrepareCamera");
-        private static readonly System.Reflection.MethodInfo k_VFXManagerProcessCameraCommand = typeof(UnityEngine.VFX.VFXManager).GetMethod("ProcessCameraCommand");
-        private static readonly bool isVFXCommandBufferSupported = k_VFXManagerPrepareCamera != null && k_VFXManagerProcessCameraCommand != null;
-
-        private static void VFXPrepareCamera(Camera camera)
-        {
-            if (isVFXCommandBufferSupported)
-            {
-                //Visual Effect Graph is not yet a required package but calling this method when there isn't any VisualEffect component has no effect (but needed for Camera sorting in Visual Effect Graph context)
-                //UnityEngine.Experimental.VFX.VFXManager.PrepareCamera(camera); 
-                k_VFXManagerPrepareCamera.Invoke(null, new[] { camera });
-            }
-            else
-            {
-                VFX.VFXManager.ProcessCamera(camera);
-            }
-        }
-
-        private static void VFXProcessCamera(Camera camera, CommandBuffer cmd)
-        {
-            if (isVFXCommandBufferSupported)
-            {
-                //UnityEngine.Experimental.VFX.VFXManager.ProcessCameraCommand(camera, cmd);
-                k_VFXManagerProcessCameraCommand.Invoke(null, new object[] { camera, cmd });
-            }
-            else
-            {
-                //Nothing (previous approach)
-            }
-        }
-
         protected override void Render(ScriptableRenderContext renderContext, Camera[] cameras)
         {
             if (!m_ValidAPI || cameras.Length == 0)
@@ -1151,7 +1120,7 @@ namespace UnityEngine.Rendering.HighDefinition
                     //  for various reasons (full screen pass through, custom render, or just invalid parameters)
                     //  and in that case the associated ending call is never called.
                     UnityEngine.Rendering.RenderPipeline.BeginCameraRendering(renderContext, camera);
-                    VFXPrepareCamera(camera);
+                    VFXManager.PrepareCamera(camera);
 
                     // Reset pooled variables
                     cameraSettings.Clear();
@@ -1707,7 +1676,7 @@ namespace UnityEngine.Rendering.HighDefinition
             SetupCameraProperties(hdCamera, renderContext, cmd);
 
             PushGlobalParams(hdCamera, cmd);
-            VFXProcessCamera(camera, cmd);
+            VFXManager.ProcessCameraCommand(camera, cmd);
 
             // TODO: Find a correct place to bind these material textures
             // We have to bind the material specific global parameters in this mode
